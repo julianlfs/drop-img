fd0203c49fc001cc136703c2dd4e3ca5eb5844bb
Julian Luís <julian.souza@deltainf.com.br>
2018-04-23 11:49:14 -0300

    ajustes

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import glob
 import shutil
 import os
 
+folder = './deleted'
 
 for img in glob.iglob('*.jpg'):
     try:
         im = Image.open(img)
         print(img + ': good')
-    except(OSError):
-        print(img + ': error')
-        if not os.path.isdir('./deleted'):
-            os.mkdir('./deleted')
-        shutil.move(img, './deleted')
-
-
-
-
+        im.close()
+    except IOError as err:
+        print('error: ' + str(err))
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+        shutil.move(img, folder)
